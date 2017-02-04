0def98afb4c04b6c3a9f87e5f3bbb6c0836ee166
dxy159 <richard.ni.wchs@gmail.com>
2017-02-03 22:31:27 -0500

    Initial Commit

---
+++ QHacks2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QHacks2017
+//
+//  Created by Richard Ni on 2017-02-03.
+//  Copyright © 2017 Richard Ni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
